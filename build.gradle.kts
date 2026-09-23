c80e632cd7e3fbb9cc6cd8953972a3a4846cf920
KAN1409 <engkareemnasser@gmail.com>
2026-09-24 02:07:14 +0300

    fix: align Kotlin compiler with LiteRT-LM 0.17.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,4 @@
-plugins { id("com.android.application") version "8.10.1" apply false
-id("org.jetbrains.kotlin.android") version "2.1.20" apply false }
+plugins {
+ id("com.android.application") version "8.10.1" apply false
+ id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+}
