9a0714e15a154c1e748b7170308aa97d1469d09c
KAN1409 <engkareemnasser@gmail.com>
2026-09-24 12:02:22 +0300

    build: fix PaddleOCR module settings newlines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,6 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
 rootProject.name="LocalOCR"
-include(":app")\ninclude(":ppocr-sdk")\nproject(":ppocr-sdk").projectDir = file("vendor/PaddleOCR/deploy/ppocr-android/ppocr-sdk")
+include(":app")
+include(":ppocr-sdk")
+project(":ppocr-sdk").projectDir = file("vendor/PaddleOCR/deploy/ppocr-android/ppocr-sdk")
