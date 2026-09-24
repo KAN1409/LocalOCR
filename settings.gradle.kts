176e7ea590d222416bfd7c7ff916dfaef0f840f7
KAN1409 <engkareemnasser@gmail.com>
2026-09-24 11:57:30 +0300

    build: wire official PaddleOCR Android SDK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
 rootProject.name="LocalOCR"
-include(":app")
+include(":app")\ninclude(":ppocr-sdk")\nproject(":ppocr-sdk").projectDir = file("vendor/PaddleOCR/deploy/ppocr-android/ppocr-sdk")
